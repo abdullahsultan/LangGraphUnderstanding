9f58be96fa021707912852ba4a3bd080991d93d2
BigBrownBear <abdullahsultan448@gmail.com>
2026-05-17 01:29:28 +0400

    nodes

---
+++ react.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain_core import tool
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: A number to be tripled.
+    Returns the triple of a number.
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+
+llm = ChatOllama(model="gemma4", temperature=0.9).bind_tools(tools)
