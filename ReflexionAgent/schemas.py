bc18d2e7e5e829128605af457d2c85078035da36
BigBrownBear <abdullahsultan448@gmail.com>
2026-05-29 20:27:49 +0400

    Revisor

--- ReflexionAgent/schemas.py
+++ ReflexionAgent/schemas.py
@@ -1,12 +1,22 @@
 from typing import List
+
 from pydantic import BaseModel, Field
+
 
 class Reflection(BaseModel):
     missing: str = Field(description="Critique of what is missing.")
     superfluous: str = Field(description="Critique of what is superfluous.")
 
+
 class AnswerQuestion(BaseModel):
     answer: str = Field(description="~250 word detailed answer to the question.")
     reflection: Reflection = Field(description="The reflection on the initial answer.")
-    search_qureries: List[str] = Field(description="1-3 search queries for searching improvement to address the critiquue of your current answer.")
-    
+    search_qureries: List[str] = Field(
+        description="1-3 search queries for searching improvement to address the critiquue of your current answer."
+    )
+
+
+class ReviseAnswer(BaseModel):
+    refrences: List[str] = Field(
+        description="Citations motivating your updated answer."
+    )
